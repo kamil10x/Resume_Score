4de9cb198e742fd7bedabcc554e2f1ec6feeb231
kamil10x <shaik.kamil10@gmail.com>
2026-06-28 10:40:56 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+require("./conn");
+
+app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin: [
+        "http://localhost:5173",
+        //"https://your-frontend-name.vercel.app"
+    ]
+}));
+
+const userRoutes = require("./Routes/user");
+const ResumeRoutes = require("./Routes/resume");
+
+app.use("/api/user", userRoutes);
+app.use("/api/resume", ResumeRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Backend is running on port ${PORT}`);
+});
