1777661b44c60a97f0ad478064ae3099b5b3d95d
kamil10x <shaik.kamil10@gmail.com>
2026-06-28 11:42:17 +0530

    Update API URL for production

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:"http://localhost:4000",
+    baseURL: "https://resume-score-a4ai.onrender.com",
+    //baseURL:"http://localhost:4000",
     //timeout:1000,
    // headers:{'Content-Type':'application/json'}
 })
 
 export default instance;
 
-//https://rusume-ai-scorer-1.onrender.com
+
+
